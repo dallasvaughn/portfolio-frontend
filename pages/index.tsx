aa4962b126dff4e97d151e8630c6baf4d4e2e7e1
Dallas Vaughn <dallasvaughn@Dallass-iMac.hsd1.wa.comcast.net>
2022-08-09 19:32:35 -0700

    added Link components

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import type { NextPage } from 'next';
 import Head from 'next/head';
 import Image from 'next/image';
+import Link from 'next/link';
 import logo from '../public/logo.svg';
 
 const Home: NextPage = () => {
@@ -13,24 +14,31 @@ const Home: NextPage = () => {
 
       <header>
         <div className="flex justify-between items-center px-5">
-          <span className="flex items-center">
-            <Image src={logo} alt="logo" width={70} height={70} />
-            <span className="text-2xl hidden sm:flex flex-col ml-4 mt-2 uppercase">
-              <strong className="font-black">Dallas</strong>{' '}
-              <span className="font-extralight ml-6 -mt-3">Vaughn</span>
+          <Link href="/">
+            <span className="flex items-center cursor-pointer">
+              <Image src={logo} alt="logo" width={70} height={70} />
+              <span className="text-2xl hidden sm:flex flex-col ml-4 mt-2 uppercase">
+                <strong className="font-black">Dallas</strong>{' '}
+                <span className="font-extralight ml-6 -mt-3">Vaughn</span>
+              </span>
             </span>
-          </span>
+          </Link>
           <nav className="flex text-sm">
-            <a
-              className="ml-4 link border-b-2 border-teal-800 rounded-sm"
-              href="/"
-            >
-              Home
-            </a>
-            <a className="ml-4 transition-all ease-in-out hover:border-b-2 ">
-              Blog
-            </a>
-            <a className="ml-4">About</a>
+            <Link href="/">
+              <span className="ml-4 link border-b-2 border-teal-800 rounded-sm cursor-pointer">
+                Home
+              </span>
+            </Link>
+            <Link href="/">
+              <span className="ml-4 transition-all ease-in-out hover:border-b-2 cursor-pointer">
+                Blog
+              </span>
+            </Link>
+            <Link href="/">
+              <span className="ml-4 transition-all ease-in-out hover:border-b-2 cursor-pointer">
+                About
+              </span>
+            </Link>
           </nav>
         </div>
       </header>
