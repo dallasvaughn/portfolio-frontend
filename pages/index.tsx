5520654ec4d40b60e580b3de895fdd2d157067a8
Dallas Vaughn <dallasvaughn@Dallass-iMac.hsd1.wa.comcast.net>
2022-08-12 18:33:12 -0700

    added animations

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,8 +4,21 @@ import Image from 'next/image';
 import Link from 'next/link';
 import logo from '../public/logo.svg';
 import crow from '../public/crow-fly.svg';
+import { useInView } from 'react-intersection-observer';
+import { useEffect } from 'react';
 
 const Home: NextPage = () => {
+  const { ref, inView, entry } = useInView();
+
+  useEffect(() => {
+    if (inView) {
+      document.getElementById('top-line')?.classList.add('top-line');
+      document.getElementById('left-line')?.classList.add('left-line');
+      document.getElementById('bottom-line')?.classList.add('bottom-line');
+      document.getElementById('right-line')?.classList.add('right-line');
+    }
+  }, [inView]);
+
   return (
     <>
       <Head>
@@ -44,15 +57,15 @@ const Home: NextPage = () => {
         </div>
       </header>
 
-      <main className="flex w-full flex-1 flex-col items-center justify-center text-center">
+      <main className="flex w-full flex-1 flex-col items-center justify-center text-center p-1">
         <div className="relative top-0 mb-20">
           <div className="h-10 sm:h-20"></div>
-          <div className="animate-slow-pulse relative opacity-20 min-h-[450px] sm:min-h-[500px] w-screen">
+          <div className="animate-fade-in relative opacity-0 min-h-[450px] sm:min-h-[500px] w-screen">
             <Image src={crow} layout="fill" />
           </div>
           <div className="absolute w-full h-full top-0 flex align-center justify-center">
             <div className="absolute w-full h-full z-0 backdrop-blur-sm"></div>
-            <div className="my-auto z-10 font-['Kanit'] text-5xl sm:text-8xl font-black uppercase flex flex-col">
+            <div className="my-auto z-10 font-['Kanit'] text-5xl sm:text-8xl font-black uppercase flex flex-col animate-fade-in-right">
               <span className="sm:text-right">Frontend</span>
               <span className="bg-neutral-300/60 border border-black py-3 sm:py-5 px-5 sm:px-10">
                 Developer
@@ -60,11 +73,56 @@ const Home: NextPage = () => {
             </div>
           </div>
         </div>
-        <div>
-          CONTENT GOES HERE CONTENT GOES HERE CONTENT GOES HERE CONTENT GOES
-          HERE CONTENT GOES HERE CONTENT GOES HERE CONTENT GOES HERE CONTENT
-          GOES HERE CONTENT GOES HERE CONTENT GOES HERE
-        </div>
+
+        <section className="w-full relative">
+          <div ref={ref} className="box sm:mt-80 left-40">
+            <svg
+              className="box-svg hidden sm:inline"
+              width="300px"
+              height="200px"
+            >
+              <line
+                id="top-line"
+                className="box-line horizontal"
+                x1="0"
+                y1="0"
+                x2="900"
+                y2="0"
+              />
+              <line
+                id="left-line"
+                className="box-line vertical"
+                x1="0"
+                y1="200"
+                x2="0"
+                y2="-400"
+              />
+              <line
+                id="bottom-line"
+                className="box-line horizontal"
+                x1="300"
+                y1="200"
+                x2="-600"
+                y2="200"
+              />
+              <line
+                id="right-line"
+                className="box-line vertical"
+                x1="300"
+                y1="0"
+                x2="300"
+                y2="600"
+              />
+            </svg>
+            <h2 className="font-['Kanit'] font-semibold text-6xl text-right uppercase mt-3 mr-2">
+              Get
+              <br />
+              in
+              <br />
+              touch
+            </h2>
+          </div>
+        </section>
       </main>
     </>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -10,12 +10,22 @@ module.exports = {
         sans: ['Biryani'],
       },
       animation: {
-        'slow-pulse': 'slowPulse 8s cubic-bezier(0.4, 0, 0.6, 1) infinite',
+        'fade-in':
+          'fadeIn 2s ease-in 0.5s forwards, slowPulse 8s 4s cubic-bezier(0.4, 0, 0.6, 1) infinite',
+        'fade-in-right': 'fadeInRight 1s ease-in-out forwards',
       },
       keyframes: {
         slowPulse: {
           '0%, 100%': { opacity: 0.2 },
           '80%': { opacity: 0.05 },
+        },
+        fadeIn: {
+          '0%': { opacity: 0 },
+          '100%': { opacity: 0.2 },
+        },
+        fadeInRight: {
+          '0%': { opacity: 0, transform: 'translateX(100%)' },
+          '100%': { opacity: 1, transform: 'translateX(0)' },
         },
       },
     },
