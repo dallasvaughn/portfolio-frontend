c1d7c6d5ab3ce5136301a224d71029d3595ea345
Dallas Vaughn <dallasvaughn@Dallass-MBP.hsd1.wa.comcast.net>
2022-09-26 20:07:45 -0700

    google analytics setup

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,8 +2,28 @@ import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
+import * as ga from '../lib/ga';
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleRouteChange = (url: string) => {
+      ga.pageview(url);
+    };
+    //When the component is mounted, subscribe to router changes
+    //and log those page views
+    router.events.on('routeChangeComplete', handleRouteChange);
+
+    // If the component is unmounted, unsubscribe
+    // from the event with the `off` method
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange);
+    };
+  }, [router.events]);
+
   return (
     <>
       <Header />
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,10 +1,27 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document';
+import gtag from '../lib/gtag';
 
 class MyDocument extends Document {
   render() {
     return (
       <Html>
         <Head>
+          <script
+            async
+            src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+          />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+              page_path: window.location.pathname,
+            });
+          `,
+            }}
+          />
           <meta
             name="description"
             content="Dallas Vaughn is a frontend engineer bringing design specifications to life in Seattle, WA"
